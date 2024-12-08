247a3116e316592d5986f9a7915373e14472343c
Maksym Poliukhovych <maxpolyuhovich@gmail.com>
2024-12-08 21:44:16 +0200

    Add animation

--- index.js
+++ index.js
@@ -1,6 +1,11 @@
 const text = document.getElementById("text");
 const textCopy = text.innerHTML;
 const work = document.getElementById("work");
+let widthCircle = 20;
+let heightCircle = 20;
+let leftPos = 11;
+let topPos = 11;
+let intervalId = 0;
 
 function StartClick(){
     text.style.padding = "0";
@@ -29,9 +34,13 @@ function StartClick(){
     anim.style.gridTemplateColumns = "1fr 1fr";
     anim.style.gridTemplateRows = "1fr 1fr";
     quad1.style.backgroundImage = "url('quad1.png')"
+    quad1.style.gridArea = "quad1";
     quad2.style.backgroundImage = "url('quad2.png')"
+    quad2.style.gridArea = "quad2";
     quad3.style.backgroundImage = "url('quad3.png')"
+    quad3.style.gridArea = "quad3";
     quad4.style.backgroundImage = "url('quad4.png')"
+    quad4.style.gridArea = "quad4";
     controls.style.maxHeight = "30px";
     controls.style.minHeight = "30px";
     controls.style.flex = "1";
@@ -44,10 +53,86 @@ function CloseClick(){
     text.innerHTML = "";
     text.innerHTML = textCopy;
     text.style.padding = "1em";
+    topPos = 11;
+    leftPos = 11;
+    widthCircle = 20;
+    heightCircle = 20;
+    clearInterval(intervalId);
 }
+
+let side = "";
 
 function StartAnimation(){
     const animButton = document.getElementById("startAnimButton");
     animButton.disabled = true;
+    const anim = document.getElementById("anim");
+    anim.innerHTML += "<div id='circle'></div>";
+    const circle = document.getElementById("circle");
+    circle.style.width = "20px";
+    circle.style.height = "20px";
+    circle.style.borderRadius = "50%";
+    circle.style.border = "solid green 2px";
+    circle.style.backgroundColor = "rgba(0, 128, 0, 0.5)";
+    circle.style.position = "relative";
+    circle.style.zIndex = "2";
+    circle.style.gridColumn = "1 / 3";
+    circle.style.gridRow = "1 / 3";
+    circle.style.top = "calc(50% - 11px)";
+    circle.style.left = "calc(50% - 11px)";
+    side = "left";
+    intervalId = setInterval(UpdateCircle, 10);
+}
 
+function UpdateCircle() {
+    const circle = document.getElementById("circle");
+    switch (side) {
+        case "left":
+            widthCircle++;
+            circle.style.width = widthCircle + "px";
+            leftPos++;
+            circle.style.left = "calc(50% - " + leftPos + "px)";
+            side = "top";
+            break;
+        case "top":
+            heightCircle++;
+            circle.style.height = heightCircle + "px";
+            topPos++;
+            circle.style.top = "calc(50% - " + topPos + "px)";
+            side = "right";
+            break;
+        case "right":
+            widthCircle++;
+            circle.style.width = widthCircle + "px";
+            side = "bottom";
+            break;
+        case "bottom":
+            heightCircle++;
+            circle.style.height = heightCircle + "px";
+            side = "left";
+            break;
+    }
+    if (widthCircle >= work.offsetWidth - 13 || heightCircle >= work.offsetHeight - 43) {
+        clearInterval(intervalId);
+        widthCircle = 20;
+        heightCircle = 20;
+        topPos = 11;
+        leftPos = 11;
+        const button = document.getElementById("startAnimButton");
+        button.id = "restartAnimButton";
+        button.innerText = "Перезапустити";
+        button.disabled = false;
+        button.onclick = RestartClick;
+    }
+}
+
+function RestartClick(){
+    const button = document.getElementById("restartAnimButton");
+    button.id = "startAnimButton";
+    button.innerText = "Почати анімацію";
+    button.onclick = StartAnimation;
+    const circle = document.getElementById("circle");
+    circle.style.width = "20px";
+    circle.style.height = "20px";
+    circle.style.top = "calc(50% - 11px)";
+    circle.style.left = "calc(50% - 11px)";
 }
