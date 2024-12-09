b9ac03f88e1babacd88ceffa6d362162dc71d1e3
Maksym Poliukhovych <maxpolyuhovich@gmail.com>
2024-12-09 17:01:28 +0200

    Add api requests

--- index.js
+++ index.js
@@ -6,6 +6,9 @@ let heightCircle = 20;
 let leftPos = 11;
 let topPos = 11;
 let intervalId = 0;
+let recordId = 1;
+let recordsFromServer;
+const recordsFromLocal = [];
 
 function StartClick(){
     text.style.padding = "0";
@@ -44,12 +47,19 @@ function StartClick(){
     controls.style.maxHeight = "30px";
     controls.style.minHeight = "30px";
     controls.style.flex = "1";
-    controls.style.justifyContent = "space-between";
+    controls.style.display = "grid";
+    controls.style.gridTemplateColumns = "1fr auto auto";
+    controls.style.gridTemplateRows = "auto";
+    controls.style.gridArea = "'info button button'";
+    controls.innerHTML += "<p id='info'>Тут буде інформація</p>";
+    const info = document.getElementById("info");
+    info.style.fontSize = "12px";
+    info.style.justifySelf = "start";
     controls.innerHTML += "<button id='startAnimButton' onclick='StartAnimation()'>Почати анімацію</button> ";
     controls.innerHTML += "<button id='closeButton' onclick='CloseClick()'>Закрити</button>";
 }
 
-function CloseClick(){
+async function CloseClick(){
     text.innerHTML = "";
     text.innerHTML = textCopy;
     text.style.padding = "1em";
@@ -58,11 +68,74 @@ function CloseClick(){
     widthCircle = 20;
     heightCircle = 20;
     clearInterval(intervalId);
+    await fetch('http://localhost:5253/api/get')
+        .then(response => response.json())
+        .then(records => {
+            console.log(records);
+            recordsFromServer = records;
+        })
+        .catch(error => console.error('Error loading objects:', error));
+    for (let i = 0; i < recordsFromServer.length; i++) {
+        recordsFromLocal.push(localStorage.getItem((i + 1).toString()));
+        localStorage.removeItem((i + 1).toString());
+    }
+    localStorage.clear();
+
+    recordId = 1;
+    text.innerHTML += "<br>";
+    const table = document.createElement("table");
+    table.style.border = "1px solid black";
+    table.style.width = "100%";
+    table.style.fontSize = "12px";
+    table.style.borderCollapse = "collapse";
+    table.style.borderSpacing = "0";
+
+    text.appendChild(table);
+
+    const thead = document.createElement("thead");
+    const headerRow = document.createElement("tr");
+    const headers = ["id", "Local", "Server"];
+    headers.forEach(header => {
+        const th = document.createElement("th");
+        th.textContent = header;
+        th.style.border = "1px solid black";
+        th.style.padding = "8px";
+        th.style.backgroundColor = "#f2f2f2";
+        headerRow.appendChild(th);
+    });
+    thead.appendChild(headerRow);
+    table.appendChild(thead);
+
+    const tbody = document.createElement("tbody");
+    for (let i = 0; i < recordsFromServer.length; i++) {
+        const row = document.createElement("tr");
+
+        const idCell = document.createElement("td");
+        idCell.textContent = recordsFromServer[i].id;
+        idCell.style.border = "1px solid black";
+        idCell.style.padding = "8px";
+        row.appendChild(idCell);
+
+        const localCell = document.createElement("td");
+        localCell.textContent = recordsFromLocal[i] ? recordsFromLocal[i] : "No message";
+        localCell.style.border = "1px solid black";
+        localCell.style.padding = "8px";
+        row.appendChild(localCell);
+
+        const serverCell = document.createElement("td");
+        serverCell.textContent = recordsFromServer[i].message;
+        serverCell.style.border = "1px solid black";
+        serverCell.style.padding = "8px";
+        row.appendChild(serverCell);
+
+        tbody.appendChild(row);
+    }
+    table.appendChild(tbody);
 }
 
 let side = "";
 
-function StartAnimation(){
+async function StartAnimation(){
     const animButton = document.getElementById("startAnimButton");
     animButton.disabled = true;
     const anim = document.getElementById("anim");
@@ -80,10 +153,24 @@ function StartAnimation(){
     circle.style.top = "calc(50% - 11px)";
     circle.style.left = "calc(50% - 11px)";
     side = "left";
+    let date = new Date();
+    document.getElementById("info").textContent = date.toLocaleString() + " - Натиснуто кнопку початку анімації";
+    const record = {
+        id: recordId.toString(),
+        message: " - Натиснуто кнопку початку анімації"
+    };
+
+    localStorage.setItem(record.id, date.toLocaleString() + record.message);
+    await SaveRecord(record);
     intervalId = setInterval(UpdateCircle, 10);
 }
 
-function UpdateCircle() {
+async function UpdateCircle() {
+    let date = new Date();
+    const record = {
+        id: recordId.toString(),
+        message: ""
+    };
     const circle = document.getElementById("circle");
     switch (side) {
         case "left":
@@ -91,6 +178,10 @@ function UpdateCircle() {
             circle.style.width = widthCircle + "px";
             leftPos++;
             circle.style.left = "calc(50% - " + leftPos + "px)";
+            document.getElementById("info").textContent = date.toLocaleString() + " - Круг розширено ліворуч";
+            record.message = " - Круг розширено ліворуч";
+            localStorage.setItem(record.id, date.toLocaleString() + record.message);
+            await SaveRecord(record);
             side = "top";
             break;
         case "top":
@@ -98,21 +189,39 @@ function UpdateCircle() {
             circle.style.height = heightCircle + "px";
             topPos++;
             circle.style.top = "calc(50% - " + topPos + "px)";
+            document.getElementById("info").textContent = date.toLocaleString() + " - Круг розширено вгору";
+            record.message = " - Круг розширено вгору";
+            localStorage.setItem(record.id, date.toLocaleString() + record.message);
+            await SaveRecord(record);
             side = "right";
             break;
         case "right":
             widthCircle++;
             circle.style.width = widthCircle + "px";
+            document.getElementById("info").textContent = date.toLocaleString() + " - Круг розширено праворуч";
+            record.message = " - Круг розширено праворуч";
+            localStorage.setItem(record.id, date.toLocaleString() + record.message);
+            await SaveRecord(record);
             side = "bottom";
             break;
         case "bottom":
             heightCircle++;
             circle.style.height = heightCircle + "px";
+            document.getElementById("info").textContent = date.toLocaleString() + " - Круг розширено вниз";
+            record.message = " - Круг розширено вниз";
+            localStorage.setItem(record.id, date.toLocaleString() + record.message);
+            await SaveRecord(record);
             side = "left";
             break;
     }
+
     if (widthCircle >= work.offsetWidth - 13 || heightCircle >= work.offsetHeight - 43) {
         clearInterval(intervalId);
+        document.getElementById("info").textContent = date.toLocaleString() + " - Круг заповнив квадранти";
+        record.message = " - Круг заповнив квадранти";
+        record.id = recordId.toString();
+        localStorage.setItem(record.id, date.toLocaleString() + record.message);
+        await SaveRecord(record);
         widthCircle = 20;
         heightCircle = 20;
         topPos = 11;
@@ -125,8 +234,16 @@ function UpdateCircle() {
     }
 }
 
-function RestartClick(){
+async function RestartClick(){
     const button = document.getElementById("restartAnimButton");
+    let date = new Date();
+    document.getElementById("info").textContent = date.toLocaleString() + " - Натиснуто кнопку перезапуску анімації";
+    const record = {
+        id: recordId.toString(),
+        message: " - Натиснуто кнопку перезапуску анімації"
+    };
+    localStorage.setItem(record.id, date.toLocaleString() + record.message);
+    await SaveRecord(record);
     button.id = "startAnimButton";
     button.innerText = "Почати анімацію";
     button.onclick = StartAnimation;
@@ -135,4 +252,19 @@ function RestartClick(){
     circle.style.height = "20px";
     circle.style.top = "calc(50% - 11px)";
     circle.style.left = "calc(50% - 11px)";
+}
+
+async function SaveRecord(message) {
+    recordId++;
+    fetch('http://localhost:5253/api/save', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(message)
+    })
+        .then(response => response.json())
+        .catch(error => {
+            console.error('Error:', error);
+        });
 }
